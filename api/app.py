890992a633a1abaca4e65c854009da9329c0af20
Beatriz Passanezi <beatriz.passanezi@gmail.com>
2026-03-09 15:50:29 +0100

    Improve structure of the files and add tests

---
+++ api/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model.score import scoring_function
+
+app = FastAPI()
+
+class ScoringRequest(BaseModel):
+    # By using pydantic, it already does data validation for us
+    address: str # required field
+    surface: float  # required field
+    num_rooms: float = None # optional field
+
+@app.post("/scoring/")
+async def get_scoring(request: ScoringRequest):
+    surface = request.surface
+    num_rooms = request.num_rooms if request.num_rooms is not None else 0
+
+    return {"score": scoring_function(surface, num_rooms)}
