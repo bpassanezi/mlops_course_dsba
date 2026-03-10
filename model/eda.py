c00b6b411b13f961efad30b77fb07687aa19457c
Chamilune31 <b00730251@essec.edu>
2026-03-10 14:51:20 +0100

    Removal eda.py

--- model/eda.py
+++
@@ -1,7 +0,0 @@
-# EDA functions have been merged into data_cleaning.py
-# Use: from model.data_cleaning import run_eda, run_all_eda
-from model.data_cleaning import run_all_eda, run_eda  # noqa: F401
-
-if __name__ == "__main__":
-    run_all_eda()
-
